fdca4bda715fea649570e6b05f870239867bdc3d
Kuchipudi Yokshith Sai Ram Chowdary <yokshithkuchipudi09@gmail.com>
2026-07-25 10:27:18 +0530

    Fix Clerk hydration crash by removing ssr: false dynamic import

--- src/components/auth/sign-in-content.tsx
+++ src/components/auth/sign-in-content.tsx
@@ -1,14 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-
-const ClerkSignIn = dynamic(
-  () => import("@clerk/nextjs").then((mod) => ({ default: mod.SignIn })),
-  { ssr: false }
-);
+import { SignIn } from "@clerk/nextjs";
 
 export function SignInPageContent() {
-  return <ClerkSignIn />;
+  return <SignIn />;
 }
--- src/components/auth/sign-up-content.tsx
+++ src/components/auth/sign-up-content.tsx
@@ -1,14 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-
-const ClerkSignUp = dynamic(
-  () => import("@clerk/nextjs").then((mod) => ({ default: mod.SignUp })),
-  { ssr: false }
-);
+import { SignUp } from "@clerk/nextjs";
 
 export function SignUpPageContent() {
-  return <ClerkSignUp />;
+  return <SignUp />;
 }
